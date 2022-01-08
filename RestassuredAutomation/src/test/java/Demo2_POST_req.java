1e3b57ff921472e77522fa54e032822014a8d02f
kundan kumar <kundanatkc@gmail.com>
2022-01-09 01:00:55 +0530

    test

--- RestassuredAutomation/src/test/java/Demo2_POST_req.java
+++ RestassuredAutomation/src/test/java/Demo2_POST_req.java
@@ -1,5 +1,44 @@
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 
 public class Demo2_POST_req {
 
-	// POST request and response
+	@SuppressWarnings("unchecked")
+	@Test
+	void PostDetails()
+	{
+		RestAssured.baseURI = "https://reqres.in/";
+		
+		RequestSpecification req = RestAssured.given();
+		
+		JSONObject js = new JSONObject();
+		
+		js.put("email","eve.holt@reqres.in");
+		js.put("password","pistol");
+//		js.put("age","23");
+//		js.put("id", "25");
+		
+		req.header("Content-Type","application/json");
+		req.body(js.toJSONString());
+		
+		Response res = req.request(Method.POST, "/api/register");
+		
+		String respBody = res.getBody().asString();
+		System.out.println("Response is "+ respBody);
+		
+		int statusCode = res.getStatusCode();
+		System.out.println("Response code is " + statusCode);
+		Assert.assertEquals(statusCode, 200);
+		
+		String sCode = res.jsonPath().get("Success Code");
+		Assert.assertEquals(sCode, "success");
+		
+	}	
 }
